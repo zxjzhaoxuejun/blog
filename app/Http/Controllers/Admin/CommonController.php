f3f3143367e909aab54244201fe1800f76c63ac9
赵学军 <zhaoxj3@zhongan.com.cn>
2017-11-07 17:14:57 +0800

    提交图片上传

--- app/Http/Controllers/Admin/CommonController.php
+++ app/Http/Controllers/Admin/CommonController.php
@@ -14,8 +14,19 @@ class CommonController extends Controller
 //    上传图片
     public function upload()
     {
-       $file=Input::file('Filedata');
-       dd($file);
+       $file=Input::file('file');
+        if ($file->isValid()){//检验上传文件是否有效
+            $clientName=$file->getClientOriginalName();//获取文件原名称
+            $realPath=$file->getRealPath();//这个表示的是缓存在tmp文件夹下的文件的绝对路径
+            $entension=$file->getClientOriginalExtension();//获取原始文件的后缀（必选）
+
+            $newName=date('YmdHis').mt_rand(100,999).'.'.$entension;
+
+            $path=$file->move(base_path().'/uploads',$newName);//移动文件路径并重新命名
+
+            $filepath='uploads/'.$newName;
+            return $filepath;
+        }
     }
 
 }
--- resources/views/admin/article/editArticle.blade.php
+++ resources/views/admin/article/editArticle.blade.php
@@ -79,23 +79,126 @@
             <tr>
                 <td width="15%" style="text-align: right">缩略图</td>
                 <td>
-                    {{--<input type="text" class="form-control" name=art_thumb"  style="width: 15%;display: inline-block">--}}
-                    <input id="file_upload"  name="art_thumb"  value="{{$filed->art_thumb}}"  style="display: inline-block" type="file" multiple="true">
-                    <script src="{{asset('uploadify/jquery.uploadify.js')}}" type="text/javascript"></script>
-                    <link rel="stylesheet" type="text/css" href="{{asset('uploadify/uploadify.css')}}">
-                    <script type="text/javascript">
-                        <?php $timestamp = time();?>
-                        $(function() {
-                            $('#file_upload').uploadify({
-                                     'formData': {
-                                    'timestamp': '<?php echo $timestamp;?>',
-                                    '_token': '{{csrf_token()}}'
-                                },
-                                'swf':'{{url('uploadify/uploadify.swf')}}' ,
-                                'uploader':'{{url('admin/upload')}}',
-                            });
-                        });
-                    </script>
+                    <link rel="stylesheet" type="text/css" href="{{ asset('uploadify/css/webuploader.css')}}" />
+            <div id="uploader" class="wu-example">
+                <div class="btns-check">
+                    <input id="art_thumb" type="text" name="art_thumb" value="" class="form-control pic-input" readonly placeholder="请选择上传图">
+                    <div id="picker" class="webuploader-container" >
+                        <div class="webuploader-pick">选择文件</div>
+                    </div>
+                    <!-- <a id="ctlBtn" class="btn btn-default" >开始上传</a> -->
+                </div>
+                <!--用来存放文件信息-->
+                <div id="thelist" class="uploader-list"></div>
+                <img src=""  id="thumb_art_img" style="max-width: 180px;max-height: 120px;">
+            </div>
+            <script type="text/javascript" src="{{ asset('uploadify/script/webuploader.js')}}"></script>
+            <script type="text/javascript">
+                // 图片上传demo
+                // 文件上传
+                $(function() {
+                    var $ = jQuery,
+                        $list = $('#thelist'),
+                        $btn = $('#ctlBtn'),
+                        state = 'pending',
+                        uploader;
+                    var thumbnailWidth=50;
+                    var thumbnailHeight=50;
+
+                    uploader = WebUploader.create({
+                        // 选完文件后，是否自动上传。
+                       auto: true,
+
+                        // 不压缩image
+                        resize: false,
+                        // swf文件路径
+                        swf: "{{ asset('uploadify/script/Uploader.swf')}}",
+                        // 文件接收服务端。
+                        server: "{{url('student/upload')}}",
+                        // 选择文件的按钮。可选。
+                        // 内部根据当前运行是创建，可能是input元素，也可能是flash.
+                        pick: '#picker',
+                        formData:{
+                            "_token":'{{csrf_token()}}'
+                      },
+                        // 只允许选择图片文件。
+                        accept: {
+                            title: 'Images',
+                            extensions: 'gif,jpg,jpeg,bmp,png',
+                            mimeTypes: 'image/*'
+                        },
+//                        
+                    });
+
+                    // 当有文件添加进来的时候
+
+                    uploader.on( 'fileQueued', function( file ) {
+                        $('.up-name-pic').remove();
+                        $list.append( '<div id="' + file.id + '" class="item up-name-pic">' +
+                            '<span class="info">' + file.name + '</span>' +
+                            '<span class="state">等待上传...</span>' +
+                            '</div>' );
+                    });
+            
+                    // 文件上传过程中创建进度条实时显示。
+                    uploader.on( 'uploadProgress', function( file, percentage ) {
+                        var $li = $( '#'+file.id ),
+                            $percent = $li.find('.progress .progress-bar');
+
+                        // 避免重复创建
+                        if ( !$percent.length ) {
+                            $percent = $('<div class="progress progress-striped active">' +
+                                '<div class="progress-bar" role="progressbar" style="width: 0%">' +
+                                '</div>' +
+                                '</div>').appendTo( $li ).find('.progress-bar');
+                        }
+
+                        $li.find('span.state').text('上传中');
+
+                        $percent.css( 'width', percentage * 100 + '%' );
+                    });
+
+                    uploader.on( 'uploadSuccess', function( file,response) {//file {File}File对象//•response {Object}服务端返回的数据
+                        console.log(response);
+                        $('#art_thumb').val(response._raw);
+                        $('#thumb_art_img').attr('src','/inputForm/'+response._raw);
+                        $('.up-name-pic').remove();
+//                        $( '#'+file.id ).find('span.state').text('已上传');
+                    });
+
+                    uploader.on( 'uploadError', function( file ) {
+                        $( '#'+file.id ).find('span.state').text('上传出错');
+                    });
+
+                    uploader.on( 'uploadComplete', function( file ) {
+                        $( '#'+file.id ).find('.progress').fadeOut();
+                    });
+
+                    uploader.on( 'all', function( type ) {
+                        if ( type === 'startUpload' ) {
+                            state = 'uploading';
+                        } else if ( type === 'stopUpload' ) {
+                            state = 'paused';
+                        } else if ( type === 'uploadFinished' ) {
+                            state = 'done';
+                        }
+
+                        if ( state === 'uploading' ) {
+                            $btn.text('暂停上传');
+                        } else {
+                            $btn.text('开始上传');
+                        }
+                    });
+
+                    $btn.on( 'click', function() {
+                        if ( state === 'uploading' ) {
+                            uploader.stop();
+                        } else {
+                            uploader.upload();
+                        }
+                    });
+                });
+            </script>
                 </td>
             </tr>
             <tr>
